a2e5cac461d474746555c424b418e9d2a7a6072e
Desenvolvedorpythonanalista <lachimolalala61@gmail.com>
2024-09-04 20:51:49 -0300

    Update calculadora_imoveis.py

--- calculadora_imoveis.py
+++ calculadora_imoveis.py
@@ -4,8 +4,7 @@ import pandas as pd
 # Configuração da interface do Streamlit
 st.set_page_config(page_title="Calculadora de Imóvel", layout="wide")
 
-def calcular_imovel(valor_inicial, valor_venda, obra, taxa_juros, entrada_percentual, numero_vendas, meses_pagar_ate_vender):
-    
+def calcular_imovel(valor_inicial, valor_venda, obra, taxa_juros, entrada_percentual, numero_vendas, meses_pagar_ate_vender, moeda):
     # Definir o período do financiamento
     periodo_financiamento = 360  # 30 anos
 
@@ -37,14 +36,25 @@ def calcular_imovel(valor_inicial, valor_venda, obra, taxa_juros, entrada_percen
     # Cálculo do gasto total
     gasto_total = valor_inicial + obra + custo_total_parcelas_ate_vender
 
+    # Ajuste do valor de venda com base na valorização (sem considerar inflação e período de meses)
+    if moeda == 'Dólar':
+        valor_venda_ajustado = valor_venda
+        simbolo_moeda = 'USD'
+    elif moeda == 'Real':
+        valor_venda_ajustado = valor_venda
+        simbolo_moeda = 'R$'
+    else:
+        valor_venda_ajustado = valor_venda  # Caso não seja uma moeda reconhecida
+        simbolo_moeda = 'R$'
+
     # Cálculo do faturamento bruto
-    faturamento_bruto = (valor_venda - gasto_total) * numero_vendas
+    faturamento_bruto = (valor_venda_ajustado - gasto_total) * numero_vendas
 
     # Cálculo da margem de lucro de 30%
     margem_lucro = faturamento_bruto * 0.30
 
     # Percentual de aumento
-    percentual_aumento = ((valor_venda - valor_inicial) / valor_inicial) * 100
+    percentual_aumento = ((valor_venda_ajustado - valor_inicial) / valor_inicial) * 100
 
     # Distribuição dos 70% do faturamento bruto
     despesas = faturamento_bruto * 0.70
@@ -89,7 +99,7 @@ def calcular_imovel(valor_inicial, valor_venda, obra, taxa_juros, entrada_percen
     resultados = {
         'Valor de Compra do Apartamento': valor_inicial,
         'Gasto Total Atualizado': gasto_total,
-        'Valor da Venda do Apartamento': valor_venda,
+        'Valor da Venda do Apartamento Ajustado': valor_venda_ajustado,
         'Faturamento Bruto': faturamento_bruto,
         'Margem de Lucro 30%': margem_lucro,
         'Percentual de Aumento no Preço de Venda': percentual_aumento,
@@ -107,7 +117,7 @@ def calcular_imovel(valor_inicial, valor_venda, obra, taxa_juros, entrada_percen
         'Material Audiovisual (15% das Despesas)': material_audiovisual
     }
 
-    return resultados, distrib_lucro_margem, despesas_detalhadas, social_media, produtora_audiovisual, gerente_comercial, contador, trafego_pago, recursos_humanos, ponto_comercial, manutencao_paginas_web, administrador, advogado_empresarial, investimentos_fixa_variavel, distribuicao_socios
+    return resultados, distrib_lucro_margem, despesas_detalhadas, social_media, produtora_audiovisual, gerente_comercial, contador, trafego_pago, recursos_humanos, ponto_comercial, manutencao_paginas_web, administrador, advogado_empresarial, investimentos_fixa_variavel, distribuicao_socios, simbolo_moeda
 
 def calcular_parcela(valor_total, taxa_juros_mensal, periodo_financiamento):
     """ Calcula o valor da parcela mensal usando a fórmula de financiamento """
@@ -115,6 +125,10 @@ def calcular_parcela(valor_total, taxa_juros_mensal, periodo_financiamento):
 
 # Configuração da interface do Streamlit
 st.title('Calculadora de Imóvel')
+
+# Barra lateral para previsões
+st.sidebar.header('Parâmetros de Valorização')
+moeda = st.sidebar.selectbox('Moeda de Valorização:', ['Dólar', 'Real'])
 
 # Inputs
 valor_inicial = st.number_input('Valor de Compra do Apartamento:', min_value=0.0, format="%.2f")
@@ -127,16 +141,21 @@ meses_pagar_ate_vender = st.number_input('Número de Meses que Prevê Pagar até
 
 if st.button('Calcular Imóvel'):
     # Calcular resultados
-    resultados, distrib_lucro_margem, despesas_detalhadas, social_media, produtora_audiovisual, gerente_comercial, contador, trafego_pago, recursos_humanos, ponto_comercial, manutencao_paginas_web, administrador, advogado_empresarial, investimentos_fixa_variavel, distribuicao_socios = calcular_imovel(valor_inicial, valor_venda, obra, taxa_juros, entrada_percentual, numero_vendas, meses_pagar_ate_vender)
+    resultados, distrib_lucro_margem, despesas_detalhadas, social_media, produtora_audiovisual, gerente_comercial, contador, trafego_pago, recursos_humanos, ponto_comercial, manutencao_paginas_web, administrador, advogado_empresarial, investimentos_fixa_variavel, distribuicao_socios, simbolo_moeda = calcular_imovel(
+        valor_inicial, valor_venda, obra, taxa_juros, entrada_percentual, numero_vendas, meses_pagar_ate_vender, moeda
+    )
 
-    # Exibição dos resultados gerais
-    st.subheader('Resultados Gerais')
-
+    # Função para formatar números
+    def formatar_numero(valor):
+        return f"{valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+    
+    # Exibição dos resultados
+    st.subheader('Resultados')
     resultados_df = pd.DataFrame.from_dict({
         'Descrição': [
-            'Gasto Total Atualizado',
             'Valor de Compra do Apartamento',
-            'Valor da Venda do Apartamento',
+            'Gasto Total Atualizado',
+            'Valor da Venda do Apartamento Ajustado',
             'Faturamento Bruto',
             'Margem de Lucro 30%',
             'Percentual de Aumento no Preço de Venda',
@@ -144,105 +163,62 @@ if st.button('Calcular Imóvel'):
             'Saldo Devedor',
             'Valor Total do Financiamento',
             'Valor das Parcelas do Financiamento',
-            'Número de Meses que Prevé Pagar até Vender',
+            'Número de Meses que Prevê Pagar até Vender',
             'Custo Total das Parcelas do Financiamento até Vender',
             'Despesas (70% do Faturamento Bruto)'
         ],
         'Valor': [
-            f'R$ {resultados["Gasto Total Atualizado"]:,.2f}',
-            f'R$ {resultados["Valor de Compra do Apartamento"]:,.2f}',
-            f'R$ {resultados["Valor da Venda do Apartamento"]:,.2f}',
-            f'R$ {resultados["Faturamento Bruto"]:,.2f}',
-            f'R$ {resultados["Margem de Lucro 30%"]:,.2f}',
-            f'{resultados["Percentual de Aumento no Preço de Venda"]:.2f}%',
-            f'R$ {resultados["Valor da Entrada"]:,.2f}',
-            f'R$ {resultados["Saldo Devedor"]:,.2f}',
-            f'R$ {resultados["Valor Total do Financiamento"]:,.2f}',
-            f'R$ {resultados["Valor das Parcelas do Financiamento"]:,.2f}',
-            f'{resultados["Número de Meses que Prevê Pagar até Vender"]} meses',
-            f'R$ {resultados["Custo Total das Parcelas do Financiamento até Vender"]:,.2f}',
-            f'R$ {resultados["Despesas (70% do Faturamento Bruto)"]:,.2f}'
+            f"{simbolo_moeda} {formatar_numero(resultados['Valor de Compra do Apartamento'])}",
+            f"{simbolo_moeda} {formatar_numero(resultados['Gasto Total Atualizado'])}",
+            f"{simbolo_moeda} {formatar_numero(resultados['Valor da Venda do Apartamento Ajustado'])}",
+            f"{simbolo_moeda} {formatar_numero(resultados['Faturamento Bruto'])}",
+            f"{simbolo_moeda} {formatar_numero(resultados['Margem de Lucro 30%'])}",
+            f"{formatar_numero(resultados['Percentual de Aumento no Preço de Venda'])}%",
+            f"{simbolo_moeda} {formatar_numero(resultados['Valor da Entrada'])}",
+            f"{simbolo_moeda} {formatar_numero(resultados['Saldo Devedor'])}",
+            f"{simbolo_moeda} {formatar_numero(resultados['Valor Total do Financiamento'])}",
+            f"{simbolo_moeda} {formatar_numero(resultados['Valor das Parcelas do Financiamento'])}",
+            f"{resultados['Número de Meses que Prevê Pagar até Vender']:.0f}",
+            f"{simbolo_moeda} {formatar_numero(resultados['Custo Total das Parcelas do Financiamento até Vender'])}",
+            f"{simbolo_moeda} {formatar_numero(resultados['Despesas (70% do Faturamento Bruto)'])}"
         ]
     })
+    st.dataframe(resultados_df)
 
-    st.dataframe(resultados_df, use_container_width=True)
+    st.subheader('Distribuição da Margem de Lucro')
+    st.write({
+        '50% para o caixa da empresa': f"{simbolo_moeda} {formatar_numero(distrib_lucro_margem['50% para o caixa da empresa'])}",
+        '50% distribuído': f"{simbolo_moeda} {formatar_numero(distrib_lucro_margem['50% distribuído'])}"
+    })
 
-    # Exibição das despesas detalhadas
     st.subheader('Despesas Detalhadas')
-
-    despesas_detalhadas_df = pd.DataFrame.from_dict({
-        'Descrição': [
-            'Investimento para o Próximo Imóvel (40% das Despesas)',
-            'Material Audiovisual (15% das Despesas)'
-        ],
-        'Valor': [
-            f'R$ {despesas_detalhadas["Investimento para o Próximo Imóvel (40% das Despesas)"]:,.2f}',
-            f'R$ {despesas_detalhadas["Material Audiovisual (15% das Despesas)"]:,.2f}'
-        ]
+    st.write({
+        'Investimento para o Próximo Imóvel (40% das Despesas)': f"{simbolo_moeda} {formatar_numero(despesas_detalhadas['Investimento para o Próximo Imóvel (40% das Despesas)'])}",
+        'Material Audiovisual (15% das Despesas)': f"{simbolo_moeda} {formatar_numero(despesas_detalhadas['Material Audiovisual (15% das Despesas)'])}"
     })
 
-    st.dataframe(despesas_detalhadas_df, use_container_width=True)
-
-    # Exibição das despesas fixas
-    st.subheader('Despesas Fixas (45% das Despesas)')
-
-    caixa_df = pd.DataFrame.from_dict({
-        'Descrição': [
-            'Social Media',
-            'Produtora Audiovisual',
-            'Gerente Comercial',
-            'Contador',
-            'Tráfego Pago',
-            'Recursos Humanos',
-            'Ponto Comercial',
-            'Manutenção Páginas Web',
-            'Administrador',
-            'Advogado Empresarial'
-        ],
-        'Valor': [
-            f'R$ {social_media:,.2f}',
-            f'R$ {produtora_audiovisual:,.2f}',
-            f'R$ {gerente_comercial:,.2f}',
-            f'R$ {contador:,.2f}',
-            f'R$ {trafego_pago:,.2f}',
-            f'R$ {recursos_humanos:,.2f}',
-            f'R$ {ponto_comercial:,.2f}',
-            f'R$ {manutencao_paginas_web:,.2f}',
-            f'R$ {administrador:,.2f}',
-            f'R$ {advogado_empresarial:,.2f}'
-        ]
+    st.subheader('Despesas Fixas')
+    st.write({
+        'Social Media': f"{simbolo_moeda} {formatar_numero(social_media)}",
+        'Produtora Audiovisual': f"{simbolo_moeda} {formatar_numero(produtora_audiovisual)}",
+        'Gerente Comercial': f"{simbolo_moeda} {formatar_numero(gerente_comercial)}",
+        'Contador': f"{simbolo_moeda} {formatar_numero(contador)}",
+        'Tráfego Pago': f"{simbolo_moeda} {formatar_numero(trafego_pago)}",
+        'Recursos Humanos': f"{simbolo_moeda} {formatar_numero(recursos_humanos)}",
+        'Ponto Comercial': f"{simbolo_moeda} {formatar_numero(ponto_comercial)}",
+        'Manutenção de Páginas Web': f"{simbolo_moeda} {formatar_numero(manutencao_paginas_web)}",
+        'Administrador': f"{simbolo_moeda} {formatar_numero(administrador)}",
+        'Advogado Empresarial': f"{simbolo_moeda} {formatar_numero(advogado_empresarial)}"
     })
 
-    st.dataframe(caixa_df, use_container_width=True)
-
-    # Exibição dos investimentos
-    st.subheader('Investimentos da Margem de Lucro (50% para o Caixa da Empresa)')
-
-    investimentos_df = pd.DataFrame.from_dict({
-        'Descrição': [
-            'Investimentos de Renda Fixa',
-            'Investimentos de Renda Variável'
-        ],
-        'Valor': [
-            f'R$ {investimentos_fixa_variavel["Investimentos de Renda Fixa"]:,.2f}',
-            f'R$ {investimentos_fixa_variavel["Investimentos de Renda Variável"]:,.2f}'
-        ]
+    st.subheader('Investimentos do Caixa da Empresa')
+    st.write({
+        'Investimentos de Renda Fixa': f"{simbolo_moeda} {formatar_numero(investimentos_fixa_variavel['Investimentos de Renda Fixa'])}",
+        'Investimentos de Renda Variável': f"{simbolo_moeda} {formatar_numero(investimentos_fixa_variavel['Investimentos de Renda Variável'])}"
     })
 
-    st.dataframe(investimentos_df, use_container_width=True)
-
-    # Exibição da distribuição para os sócios
-    st.subheader('Distribuição da Margem de Lucro (50% distribuído)')
-
-    distribuicao_socios_df = pd.DataFrame.from_dict({
-        'Descrição': [
-            'Sócio 1',
-            'Sócio 2'
-        ],
-        'Valor': [
-            f'R$ {distribuicao_socios["Sócio 1"]:,.2f}',
-            f'R$ {distribuicao_socios["Sócio 2"]:,.2f}'
-        ]
+    st.subheader('Distribuição para Sócios')
+    st.write({
+        'Sócio 1': f"{simbolo_moeda} {formatar_numero(distribuicao_socios['Sócio 1'])}",
+        'Sócio 2': f"{simbolo_moeda} {formatar_numero(distribuicao_socios['Sócio 2'])}"
     })
-
-    st.dataframe(distribuicao_socios_df, use_container_width=True)
